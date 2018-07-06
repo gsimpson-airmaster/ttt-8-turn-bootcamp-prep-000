68e622da046254c776c09f3c745171022d15dba0
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-06 12:12:06 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  if(index == nil || board[index] == nil)
+    return FALSE
+  end
+  if(board[index].upcase == "X" || board[index].upcase == "O")
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def valid_move?(board, index)
+  if(index < 0 || index > 9 || index == nil)
+    return FALSE
+  elsif(position_taken?(board, index))
+    return FALSE
+  else
+    return TRUE
+  end
+end
+
